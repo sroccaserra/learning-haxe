be33bb576c0336b61057ccc0ff0f58518141cf37
sroccaserra <sroccaserra@yahoo.com>
2023-09-05 08:25:50 +0200

    r Extract initFrames function

--- src/Player.hx
+++ src/Player.hx
@@ -1,40 +1,42 @@
 class Player {
+    static var PW = 16;
+    static var PH = 16;
+
     var anim: h2d.Anim;
     var stillFrames: Array<h2d.Tile>;
     var runningFrames: Array<h2d.Tile>;
 
     public function new(game: Game, x: Float, y: Float) {
-        var PW = 16;
-        var PH = 16;
-        stillFrames = [game.tileSheet.sub(0, 0, PW, PH)];
-        runningFrames =
-            [for (i in 0...2) game.tileSheet.sub(0, i*PW, PW, PH)] ;
+        initFrames(game.tileSheet);
         anim = new h2d.Anim(stillFrames, 5);
         anim.x = x;
         anim.y = y;
         game.world.add(anim, Game.LAYER_SPRITES);
     }
 
+    private function initFrames(tileSheet: h2d.Tile) {
+        stillFrames = [tileSheet.sub(0, 0, PW, PH)];
+        runningFrames = [for (i in 0...2) tileSheet.sub(0, i*PW, PW, PH)];
+    }
+
     public function update(dt: Float, input: Input) {
-        var isMoving = false;
+        var isRunning = false;
         if (input.up) {
             anim.y -= 1;
-            isMoving = true;
         }
         if (input.down) {
             anim.y += 1;
-            isMoving = true;
         }
         if (input.left) {
             anim.x -= 1;
-            isMoving = true;
+            isRunning = true;
         }
         if (input.right) {
             anim.x += 1;
-            isMoving = true;
+            isRunning = true;
         }
 
-        if (isMoving) {
+        if (isRunning) {
             anim.play(runningFrames, anim.currentFrame);
         }
         else {
