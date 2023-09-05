e5c617179420183e0738eee108f1770e5db00bad
sroccaserra <sroccaserra@yahoo.com>
2023-09-05 08:22:26 +0200

    f Play running anim when moving only

--- src/Player.hx
+++ src/Player.hx
@@ -1,31 +1,44 @@
 class Player {
-    var object: h2d.Object;
+    var anim: h2d.Anim;
+    var stillFrames: Array<h2d.Tile>;
+    var runningFrames: Array<h2d.Tile>;
 
     public function new(game: Game, x: Float, y: Float) {
-
         var PW = 16;
         var PH = 16;
-        var runningAnim = new h2d.Anim(
-                [for (i in 0...2) game.tileSheet.sub(0, i*PW, PW, PH)],
-                5);
-        object = runningAnim;
-        object.x = x;
-        object.y = y;
-        game.world.add(object, Game.LAYER_SPRITES);
+        stillFrames = [game.tileSheet.sub(0, 0, PW, PH)];
+        runningFrames =
+            [for (i in 0...2) game.tileSheet.sub(0, i*PW, PW, PH)] ;
+        anim = new h2d.Anim(stillFrames, 5);
+        anim.x = x;
+        anim.y = y;
+        game.world.add(anim, Game.LAYER_SPRITES);
     }
 
     public function update(dt: Float, input: Input) {
+        var isMoving = false;
         if (input.up) {
-            object.y -= 1;
+            anim.y -= 1;
+            isMoving = true;
         }
         if (input.down) {
-            object.y += 1;
+            anim.y += 1;
+            isMoving = true;
         }
         if (input.left) {
-            object.x -= 1;
+            anim.x -= 1;
+            isMoving = true;
         }
         if (input.right) {
-            object.x += 1;
+            anim.x += 1;
+            isMoving = true;
+        }
+
+        if (isMoving) {
+            anim.play(runningFrames, anim.currentFrame);
+        }
+        else {
+            anim.play(stillFrames, anim.currentFrame);
         }
     }
 }
